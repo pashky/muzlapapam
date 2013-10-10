4aed087582d78b63a4724c273a93f7eca8b2b9a5
Pavel Kurnosov <pashky@gmail.com>
2013-10-10 14:53:16 +0400

    Initial version

---
+++ README.md
@@ -0,0 +1,17 @@
+# WTF?
+
+Why is it so fucking hard? There're non-iPod MP3 players out there,
+and it's okay to have music in iTunes. Yet all that 3rd party
+android/wp8/whatever sync junkware tries to be iTunes replacement and
+can't do one simple task - get songs out of existing playlists and put
+them according to tags in AlbumArtist/Album/Song folder
+structure. Skip files if exist already.
+
+So this script does just that. And extracts album artwork into
+cover.jpg too, to make rockbox happy (it can't extract artwork out of
+M4A container anyway).
+
+# How?
+
+Tweak variables on top and run. Simple.
+
---
+++ sync.rb
@@ -0,0 +1,91 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'appscript'
+require 'fileutils'
+require 'RMagick'
+require 'set'
+include Magick
+
+export_dir = '/Volumes/SANSA64/Library'
+playlist_dir = '/Volumes/SANSACLIPZ/Playlists'
+playlist_prefix = '/<microSD1>/Library'
+
+playlists = ['shuffle','export']
+
+#### no config beyond this point
+
+processed_tracks = Set.new
+processed_artworks = Set.new
+
+itu = Appscript.app("iTunes.app")
+whose = Appscript.its
+library = itu.sources[whose.kind.eq(:library)][1]
+
+def safename(t)
+  t.gsub(/[\/\\]/, '_'); 
+end
+
+playlists.each do |playlist_name|
+  puts "Processing playlist #{playlist_name}"
+  
+  playlist = library.user_playlists[whose.name.eq(playlist_name)][1]
+
+  tracks = playlist.tracks.get
+  total_tracks = tracks.size
+
+  playlist_files = []
+  tracks.each_with_index do |track, i_track|
+    id = track.database_ID.get
+    next if processed_tracks.include?(id)
+    processed_tracks.add(id)
+    
+    album_artist = track.album_artist.get
+    album_artist = track.artist.get if album_artist.empty?
+    album = track.album.get
+    year = track.year.get
+    album = 'Unknown' if album.empty?
+    album = "#{year} #{album}" if year > 0
+    name = track.name.get
+    num = track.track_number.get
+    name = "#{num} #{name}" if num > 0
+
+    location = track.location.get.path
+    extension = File.extname(location)
+
+    relative_filepath = File.join([album_artist, album, name + extension].map{|x| safename(x)})
+    playlist_files.push(relative_filepath)
+    
+    filepath = File.join(export_dir, relative_filepath)
+    albumpath = File.dirname(filepath)
+
+    percent = i_track * 100 / total_tracks;
+    puts "[#{percent}%] #{filepath}"
+
+    unless processed_artworks.include?(albumpath) then
+      processed_artworks.add(albumpath)
+      
+      FileUtils.mkpath(albumpath)
+      artworkpath = File.join(albumpath, 'cover.jpg')
+      if !File.exists?(artworkpath) then
+        begin 
+          image = Image.from_blob(track.artworks[1].data_.get.data).first
+          image.change_geometry('300x300') { |cols, rows, img| img.resize!(cols, rows) }
+          image.write(artworkpath)
+        rescue
+          # say something?
+        end
+      end
+    end
+
+
+    if !File.exists?(filepath) || File.size(filepath) != File.size(location) then
+      FileUtils.cp location, filepath
+    end
+
+  end
+
+  playlistpath = File.join(playlist_dir, safename(playlist_name) + ".m3u8")
+  File.open(playlistpath, "w") { |f| f.puts playlist_files.map{|n| File.join(playlist_prefix, n)} }
+end
+
+puts "Complete"
